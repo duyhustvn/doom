6611e0931406bd8b0250fc44e32ddd9b1262ccd2
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2023-05-18 23:23:49 +0700

    ssh agent

--- exwm/start-exwm.sh
+++ exwm/start-exwm.sh
@@ -5,14 +5,12 @@ if [ -z "$SSH_AUTH_SOCK" ] ; then
    eval `ssh-agent -s`
 fi
 
-
-
 # Set up ssh-agent and keychain in Emacs
-if [ -n "$DISPLAY" ] && [ -z "$SSH_AUTH_SOCK" ] && [ -f "/usr/bin/gnome-keyring-daemon" ]; then
-  eval $(/usr/bin/gnome-keyring-daemon --start --components=ssh)
-  export SSH_AUTH_SOCK=$(echo $SSH_AUTH_SOCK | sed "s:/run/user/$UID/keyring/ssh$::")
-  emacsclient -e "(setenv \"SSH_AUTH_SOCK\" \"$SSH_AUTH_SOCK\")" >/dev/null
-fi
+# if [ -n "$DISPLAY" ] && [ -z "$SSH_AUTH_SOCK" ] && [ -f "/usr/bin/gnome-keyring-daemon" ]; then
+#   eval $(/usr/bin/gnome-keyring-daemon --start --components=ssh)
+#   export SSH_AUTH_SOCK=$(echo $SSH_AUTH_SOCK | sed "s:/run/user/$UID/keyring/ssh$::")
+#   emacsclient -e "(setenv \"SSH_AUTH_SOCK\" \"$SSH_AUTH_SOCK\")" >/dev/null
+# fi
 
 # run command 'xinput list' to determine the ID/name of touchpad device
 # run command 'xinput -list-props ID' to list properties of device
