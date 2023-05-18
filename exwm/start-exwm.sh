fe5a30901987237f8c0d5d414ee0f23d7169b3cc
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2023-05-18 23:15:47 +0700

    ssh agent

--- exwm/start-exwm.sh
+++ exwm/start-exwm.sh
@@ -5,7 +5,6 @@ if [ -z "$SSH_AUTH_SOCK" ] ; then
    eval `ssh-agent -s`
 fi
 
-
 # Set up ssh-agent and keychain in Emacs
 if [ -n "$DISPLAY" ] && [ -z "$SSH_AUTH_SOCK" ] && [ -f "/usr/bin/gnome-keyring-daemon" ]; then
   eval $(/usr/bin/gnome-keyring-daemon --start --components=ssh)
